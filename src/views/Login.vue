da4d9f58f4337b97654a4cae52451b92c462a704
Alireza Davoodi <alirezadavoodi1378@gmail.com>
2023-05-08 15:05:57 +0430

    minor change

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -195,7 +195,7 @@ export default {
                         <label class="label justify-start">
                             <i class='bx bxl-gmail mr-2'></i><span class="label-text">{{ lang[selectedLang].email }}</span>
                         </label>
-                        <input v-model="email" class="input input-bordered" placeholder="email" type="text" />
+                        <input v-model="email" class="input input-bordered" placeholder="email" type="email" />
                     </div>
                     <div class="form-control">
                         <label class="label justify-start">
