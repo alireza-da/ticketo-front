a10260763335460f94a0a5e32f62479cd8281b2d
Alireza Davoodi <alirezadavoodi1378@gmail.com>
2024-11-05 12:32:59 +0330

    update google clientId

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ const app = createApp(App).use(router).use(Notifications)
 
 
 app.use(createPinia())
-app.use(vue3GoogleLogin, {clientId: '630380461272-molk606qisnjg3p903qubt6htp20feob.apps.googleusercontent.com', popupType: 'TOKEN', prompt: false})
+app.use(vue3GoogleLogin, {clientId: '789140008513-3d5egqt5b78km6l0aa1nsss5nh17chnq.apps.googleusercontent.com', popupType: 'TOKEN', prompt: false})
 
 app.mount('#app')
 
